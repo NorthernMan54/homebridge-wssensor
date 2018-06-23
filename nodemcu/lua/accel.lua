8560ba8c01129fd09c502c431a7e0299c866b180
NorthernMan54 <buyminivan-northern@yahoo.ca>
2018-06-23 17:04:29 -0400

    Stable

--- nodemcu/lua/accel.lua
+++ nodemcu/lua/accel.lua
@@ -1,6 +1,7 @@
 local module = {}
 
 local mpu = require('mpu6050')
+local ws
 
 function module.start(wsserver)
   local tm = tmr.now()
@@ -17,6 +18,7 @@ function module.start(wsserver)
   ws:on("receive", function(sck, msg, opcode)
     print('\ngot message:', msg, opcode) -- opcode is 1 for text message, 2 for binary
     sck:send(mpu.read(), 1)
+    tmr.softwd(600)
   end)
   ws:on("close", function(_, status)
     print('connection closed', status)
@@ -28,18 +30,13 @@ function module.start(wsserver)
 
 
   function motionEvent(value, interval)
-
-    print("Heap Available: event  " .. node.heap() )
     -- Ignore sensor for first minute
     if tmr.time() > 1 then
       if value == last then
         print("Motion Event - False")
       else
         if connected == true then
-          print("Motion Event", value, math.floor((tmr.now() - tm) / 1000000 + 0.5))
-          tm = tmr.now()
-
-          print("Heap Available: -sensors  " .. node.heap() )
+          print("\nMotion Event", value, interval)
           ws:send(mpu.read(value, interval), 1)
         else
           print( "Motion event not sent, no connection")
@@ -58,7 +55,7 @@ function module.start(wsserver)
 
     local trigger = false
     local status = nil
-    if (tmr.time() - interval) > -1 -- Minimum event length is 1 second
+    if (tmr.time() - interval) > 1 -- Minimum event length is 1 second
     then
       uart.write(0, "-")
       local _movementA, _movementG, _Temperature = mpu.rawRead()
--- nodemcu/lua/test.lua
+++ nodemcu/lua/test.lua
@@ -1,23 +1,10 @@
-local function dump(o)
-  if type(o) == 'table' then
-    local s = '{ '
-    for k, v in pairs(o) do
-      if type(k) ~= 'number' then k = '"'..k..'"' end
-      s = s .. '['..k..'] = ' .. dump(v) .. ','
-    end
-    return s .. '} '
-  else
-    return tostring(o)
-  end
-end
-
 local lua_mdns = nil
 
 local function hb_found(ws)
   print("WS Socket available http://"..ws.ipv4..":"..ws.port)
   lua_mdns = nil
-  print("Cancelling watch dog")
-  tmr.softwd(-1)
+  print("Reset watch dog")
+  tmr.softwd(600)
   led.connected()
 
   collectgarbage()
