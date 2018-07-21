63f8f6f1cf631ba74b66f002b56a2abf22699488
NorthernMan54 <buyminivan-northern@yahoo.ca>
2018-07-21 09:36:37 -0400

    Pins

--- nodemcu/lua/config.lua
+++ nodemcu/lua/config.lua
@@ -2,22 +2,22 @@ local module = {}
 
 -- Options are DHT or DHT-YL, used by homebridge to determine if moisture data is valid.
 module.Model = "BME-GD"
-module.Version = "2.1"
+module.Version = "2.2"
 
 module.ID = wifi.sta.gethostname()
 
 -- LED state
 module.ledState = 1 -- 0: fully disabled, 1: LEDs on, 2: Connected off (Boot/Error only)
 
-module.ledRed = 0 -- gpio16
-module.ledBlue = 4 -- gpio2
-module.sensor = 7 -- gpio14
+module.ledRed = 0   -- gpio16
+module.ledBlue = 4  -- gpio2
+module.sensor = 7   -- gpio14
 
 -- Garage Door relay and status sensors
 
 module.gdrelay = 2    -- Relay to control garage door
-module.gdclosed = 3   -- Magnetic reed sensor to detect door closed state
-module.gdopened = 1     -- Magnetic reed sensor to detect door open state
+module.gdclosed = 7   -- Magnetic reed sensor to detect door closed state
+module.gdopened = 1   -- Magnetic reed sensor to detect door open state
 
 -- BME280 sensor
 
@@ -27,12 +27,12 @@ module.bme280sda = 6  -- D6
 module.mpu6050scl = 6  -- D5 and D6 didn't work with the MPU 6050 for some reason
 module.mpu6050sda = 7  --
 
-module.DHT22 = 2 -- D2
-module.YL69 = 0 -- adc pin 0
-module.YL69P = 7 -- 5 with DHT and 7 with BME
+module.DHT22 = 2    -- D2
+module.YL69 = 0     -- adc pin 0
+module.YL69P = 7    -- 5 with DHT and 7 with BME
 
-module.SC501 = 2 -- AM312 Motion Sensor
-module.SW420 = 2 -- SW-420 Vibration Sensor
+module.SC501 = 2    -- AM312 Motion Sensor
+module.SW420 = 2    -- SW-420 Vibration Sensor
 
 -- Hardware Timers
 -- 1 - Used by LED to flash LED's during setup
